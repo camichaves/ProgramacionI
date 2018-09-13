98d359b801572a04f876efb98a030d6ce0fae1fb
CAMILA-PC\CAMILA <camichavesthg@gmail.com>
2018-09-13 00:35:37 -0300

    ej 3 y 4  del tp1

---
+++ TP1/ej3.php
@@ -0,0 +1,43 @@
+<?php
+$var2="a";
+$var=1;
+$var3="r";
+$var4="a1";
+$var5="a01";
+
+for($i=1;$i<10;$i++){
+    $var++;
+    echo "Mi var=1 --> $var <br>";
+    $var2++;
+    echo "Mi var a --> $var2 <br>";
+    $var3++;
+    echo "Mi var r --> $var3 <br>";
+    $var4++;
+    echo "Mi var a1 --> $var4 <br>";
+    $var5++;
+    echo "Mi var a01 --> $var5 <br>";
+
+
+}
+
+/**
+ * Created by PhpStorm.
+ * User: CAMILA
+ * Date: 13/09/2018
+ * Time: 12:22 AM
+ *
+ *
+ * Definir	en	un	script:
+a- Una	variable	con	valor	1,	incrementarla	diez	veces	y	mostrar	el	contenido	por	pantalla
+(cada	vez	que	se	incrementa).
+b- Una	variable	con	valor	“a”,	incrementarla	diez	veces	y	mostrar	el	contenido	por	pantalla
+(cada	vez	que	se	incrementa).
+c- Una	variable	con	valor	“r”,	incrementarla	diez	veces	y	mostrar	el	contenido	por	pantalla
+(cada	vez	que	se	incrementa).
+d- Una	 variable	 con valor	 “a1”,	 incrementarla	 diez	 veces	 y	 mostrar	 el	 contenido	 por
+pantalla (cada	vez	que	se	incrementa).
+e- Una	 variable	 con	 valor	 “a01”,	 incrementarla	 diez	 veces	 y	 mostrar	 el	 contenido	 por
+pantalla	(cada	vez	que	se	incrementa).
+f- Una	 variable	 con	 valor	 “a90”,	 incrementarla	 diez	 veces	 y	 mostrar	 el	 contenido	 por
+pantalla	(cada	vez	que	se	incrementa).
+ */
---
+++ TP1/ej4.php
@@ -0,0 +1,20 @@
+<?php
+
+$cad="2,34,5,6,5,45";
+print_r($cad);
+echo "<br>";
+$num=explode(",",$cad);
+print_r($num);
+/**
+ * Created by PhpStorm.
+ * User: CAMILA
+ * Date: 13/09/2018
+ * Time: 12:31 AM
+ *
+ *
+ * Definir	en	un	script:
+a- Un	 string	 con	 números	 separados	 por	 coma,	mostrar	 su	 valor,	 convertirlo	a	arreglo	 y
+mostrar	el	contenido	del	arreglo.
+b- Un	arreglo	de	números,	mostrar	sus	valores,	convertirlo	a	string	separando	los	números
+con	coma	y	mostrar	el	valor	del	string.
+ */
